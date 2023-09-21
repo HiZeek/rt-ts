ea6fb66bd9a965e65ae56af8f01a330e1329702c
Isaac Idibia <idibiaisaac3@gmail.com>
2023-09-21 18:53:34 +0100

    setup

---
+++ src/components/Todos.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+const Todos = (props: Props) => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Learn TypeScript</li>
+    </ul>
+  );
+};
+
+export default Todos;
